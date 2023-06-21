8026b5c9d3b24219cf6691fbf86ac4c1297519a3
laolang2016 <xiaodaima2016@163.com>
2023-06-21 23:22:55 +0800

    ✨ feat(static-prototype): 右侧状态与总览切换

--- static-prototype/src/js/reports.js
+++ static-prototype/src/js/reports.js
@@ -8,4 +8,29 @@ $(()=>{
         $('.content-list>.content').removeClass('active')
         $('.content-list>.content').eq($(this).index()).addClass('active')
     })
+
+    // dashboard 切换
+    $('#statistics-toggle').on('click',function(){
+        $(this).toggleClass('active')
+        $('.statistics').toggleClass('active')
+    })
+
+    // 成功失败过滤
+    $('.filter-toggle').hover(function(){
+        $('.filter-box').addClass('active')
+    },function(){
+        $('.filter-box').removeClass('active')
+    })
+
+    $('#filter-success').on('click',function(){
+        console.log('success')
+    })
+    $('#filter-failed').on('click',function(){
+        console.log('failed')
+    })
+    $('#filter-all').on('click',function(){
+        console.log('all')
+    })
+
+    $('#statistics-toggle').click()
 })
