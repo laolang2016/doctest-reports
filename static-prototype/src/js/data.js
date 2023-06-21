7e7d43d531c2ac0b4d22ba6c557a664462a8c282
laolang2016 <xiaodaima2016@163.com>
2023-06-22 07:44:28 +0800

    ✨ feat(static-prototype): 动态生成结构

---
+++ static-prototype/src/js/data.js
@@ -0,0 +1,16 @@
+const dashboardData = {
+    "suite":{
+        name:"test_calc",
+        "case":{
+            name:"test_calc_add_1"
+        }
+    },
+    "assert":{
+        "success": 4,
+        "failed": 2
+    },
+    "case":{
+        "success": 4,
+        "failed": 2
+    }
+}
