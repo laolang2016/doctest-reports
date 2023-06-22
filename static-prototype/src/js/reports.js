53e5d0354edd8fe896c9f6af12fcdcffa7392023
laolang2016 <xiaodaima2016@163.com>
2023-06-22 14:01:49 +0800

    ✨ feat(static-prototype): dashboard 页面数据联动

--- static-prototype/src/js/reports.js
+++ static-prototype/src/js/reports.js
@@ -75,8 +75,178 @@ $(() => {
     // ---------------------------------------
     $('#statistics-toggle').click()
 
-    drawStatistics(7, 3, 'suite-chart')
-    drawStatistics(7, 3, 'case-chart')
-    drawStatistics(60, 40, 'assert-chart')
+    
+    drawStatistics(dashboardData.suiteAccount.success, dashboardData.suiteAccount.failed, 'suite-chart')
+    drawStatistics(dashboardData.assertCount.success, dashboardData.assertCount.failed, 'case-chart')
+    drawStatistics(dashboardData.caseCount.success, dashboardData.caseCount.failed, 'assert-chart')
 
+    $('#suite-success-count').text(dashboardData.suiteAccount.success)
+    $('#suite-failed-count').text(dashboardData.suiteAccount.failed)
+    $('#case-success-count').text(dashboardData.assertCount.success)
+    $('#case-failed-count').text(dashboardData.assertCount.failed)
+    $('#assert-success-count').text(dashboardData.caseCount.success)
+    $('#assert-failed-count').text(dashboardData.caseCount.failed)
+
+
+    dashboardData.suites.forEach(suite => {
+        // suite 列表
+        const arr = new Array()
+        arr.push(`<div class="suite-item" case-list-info="${suite.case_list_info.id}">`)
+        arr.push(`<span class="suite-name">${suite.name}</span>`)
+        arr.push('<span class="tip">')
+        arr.push(`<i class="pass">${suite.case_success}</i>`)
+        arr.push(`<i class="failed">${suite.case_failed}</i>`)
+        arr.push('</span>')
+        arr.push('</div>')
+        $('#test-suite').append(arr.join("\n"))
+
+        // test case 列表
+
+        suite.case_list_info.case_list.forEach(caseInfo => {
+            const arr = new Array()
+            if (caseInfo.success) {
+                arr.push(`<div class="case-item" success="1" case-list-id=${suite.case_list_info.id}>`)
+            } else {
+                if (caseInfo.has_subcase) {
+                    arr.push(`<div class="case-item" success="0" case-list-id=${suite.case_list_info.id} list-type="subcase" subcase-list-info=${caseInfo.subcases_info_list.id}>`)
+                } else {
+                    arr.push(`<div class="case-item" success="0" case-list-id=${suite.case_list_info.id} list-type="expression" expression-list-info=${caseInfo.expressions_list.id}>`)
+                }
+            }
+
+            arr.push(`<span class="case-name">${caseInfo.name}<i class="source-line">${caseInfo.source_line}</i></span>`)
+            arr.push(`<span class="tip">`)
+            if (caseInfo.success) {
+                arr.push(`<span class="iconfont icon-duigou success"></span>`)
+                arr.push(`</span>`)
+                arr.push(`</div>`)
+            } else {
+                if (caseInfo.has_subcase) {
+                    arr.push(`<i class="pass-count">${caseInfo.subcase_success}</i>`)
+                    arr.push(`<i class="failed-count">${caseInfo.subcase_failed}</i>`)
+                    arr.push(`</span>`)
+                    arr.push(`</div>`)
+                } else {
+                    arr.push(`<span class="iconfont icon-duigou failed"></span>`)
+                    arr.push(`</span>`)
+                    arr.push(`</div>`)
+                }
+            }
+            $('#test-case').append(arr.join("\n"))
+
+            if (caseInfo.has_expressions) {
+                caseInfo.expressions_list.expressions.forEach(expression => {
+                    const arr = new Array()
+                    arr.push(`<div class="detail-item" expressions-list-id=${caseInfo.expressions_list.id}>`)
+                    arr.push(`<div class="expr">`)
+                    arr.push(`<span>${expression.original}</span>`)
+                    arr.push(`<span>${expression.expanded}</span>`)
+                    arr.push(`</div>`)
+                    arr.push(`<div class="source-line">`)
+                    arr.push(`${expression.source_line}`)
+                    arr.push(`</div>`)
+                    if (expression.info) {
+                        arr.push(`<div class="info">`)
+                        arr.push(`${expression.info}`)
+                        arr.push(`</div>`)
+                    }
+                    arr.push(`</div>`)
+                    $('#detail').append(arr.join("\n"))
+                })
+            }
+
+            if (caseInfo.has_subcase) {
+                caseInfo.subcases_info_list.subcase_list.forEach(subcase => {
+                    const arr = new Array()
+                    if (subcase.success) {
+                        arr.push(`<div class="subcase-item" success="1" subcase-list-id=${caseInfo.subcases_info_list.id}>`)
+                    } else {
+                        arr.push(`<div class="subcase-item" success="0" subcase-list-id=${caseInfo.subcases_info_list.id} expression-list-info=${subcase.expressions_list.id}>`)
+                    }
+
+                    arr.push(`<span class="subcase-name">${subcase.name}<i class="source-line">${subcase.source_line}</i></span>`)
+                    arr.push(`<span class="tip">`)
+                    if (!subcase.success) {
+                        arr.push(`<span class="iconfont icon-cuowu failed"></span>`)
+                    } else {
+                        arr.push(`<span class="iconfont icon-duigou success"></span>`)
+                    }
+                    arr.push(`</span>`)
+                    arr.push(`</div>`)
+                    $('#subcase').append(arr.join("\n"))
+
+                    if (subcase.has_expressions) {
+                        subcase.expressions_list.expressions.forEach(expression => {
+                            const arr = new Array()
+                            arr.push(`<div class="detail-item" expressions-list-id=${subcase.expressions_list.id}>`)
+                            arr.push(`<div class="expr">`)
+                            arr.push(`<span>${expression.original}</span>`)
+                            arr.push(`<span>${expression.expanded}</span>`)
+                            arr.push(`</div>`)
+                            arr.push(`<div class="source-line">`)
+                            arr.push(`${expression.source_line}`)
+                            arr.push(`</div>`)
+                            if (expression.info) {
+                                arr.push(`<div class="info">`)
+                                arr.push(`${expression.info}`)
+                                arr.push(`</div>`)
+                            }
+                            arr.push(`</div>`)
+                            $('#detail').append(arr.join("\n"))
+                        })
+                    }
+                })
+            }
+        })
+
+    })
+
+
+    
+
+    $('.subcase-item').on('click', function () {
+        if ('1' === $(this).attr('success')) {
+            $('.detail-item').removeClass('show')
+        } else {
+            const expressionListId = $(this).attr('expression-list-info')
+            const selector = `div[expressions-list-id='${expressionListId}']`
+            $("div[expressions-list-id^='expressions_list_id']").removeClass('show')
+            $(selector).addClass('show')
+        }
+    })
+
+    $('.case-item').on('click', function () {
+        if ('1' === $(this).attr('success')) {
+            $('.subcase-item').removeClass('show')
+            $('.detail-item').removeClass('show')
+        } else {
+            if ('subcase' === $(this).attr('list-type')) {
+                const subcaseListId = $(this).attr('subcase-list-info')
+                const selector = `div[subcase-list-id='${subcaseListId}']`
+                $("div[subcase-list-id^='subcase_list_id']").removeClass('show')
+                $(selector).addClass('show')
+                $(selector).eq(0).click()
+                $(this).addClass('active').siblings().removeClass('active')
+            } else if ('expression' === $(this).attr('list-type')) {
+                const expressionListId = $(this).attr('expression-list-info')
+                const selector = `div[expressions-list-id='${expressionListId}']`
+                $("div[expressions-list-id^='expressions_list_id']").removeClass('show')
+                $(selector).addClass('show')
+                $('.subcase-item').removeClass('show')
+            }
+        }
+    })
+
+    $('.suite-item').on('click', function () {
+        const caseListId = $(this).attr('case-list-info')
+        const selector = `div[case-list-id='${caseListId}']`
+        $("div[case-list-id^='case_list_id']").removeClass('show')
+        $(selector).addClass('show')
+        $(selector).eq(0).click()
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+
+    
+
+    $('.suite-item').eq(0).click()
 })
